fb1a3e5b2b013b8a823b789bacb95f11e1f7a45e
raju12341234 <rajudikar2010@gmail.com>
2025-07-18 12:41:16 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBG6-3GFkFX10m6nbpUXWWlN4doDhzX2zQ",
+  authDomain: "project-25058042795908309.firebaseapp.com",
+  databaseURL: "https://project-25058042795908309-default-rtdb.firebaseio.com",
+  projectId: "project-25058042795908309",
+  storageBucket: "project-25058042795908309.firebasestorage.app",
+  messagingSenderId: "458596677207",
+  appId: "1:458596677207:web:db69c0148ec25ca3e31512",
+};
